181ebcc798f1b2589c1be23f645ba136f417b87e
Alec <alec4nandes@gmail.com>
2024-03-10 11:36:53 -0700

    feature: add pw reset button

--- src/sign-in.js
+++ src/sign-in.js
@@ -1,13 +1,38 @@
 import { auth } from "./db.js";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import {
+    sendPasswordResetEmail,
+    signInWithEmailAndPassword,
+} from "firebase/auth";
 
-document.querySelector("form#sign-in").onsubmit = handleSignIn;
+const signInForm = document.querySelector("form#sign-in");
+
+signInForm.onsubmit = handleSignIn;
+document.querySelector("button#reset-pw").onclick = handleResetPassword;
 
 async function handleSignIn(e) {
     e.preventDefault();
     const { email, password } = e.target;
     try {
         await signInWithEmailAndPassword(auth, email.value, password.value);
+    } catch (err) {
+        console.error(err);
+        alert(err.message);
+    }
+}
+
+async function handleResetPassword() {
+    try {
+        const email = signInForm.email.value.trim();
+        console.log(email);
+        if (email) {
+            await sendPasswordResetEmail(auth, email);
+            alert(`Password reset email sent to ${email}`);
+        } else {
+            alert(
+                "Please enter your email address " +
+                    "to receive a password reset email."
+            );
+        }
     } catch (err) {
         console.error(err);
         alert(err.message);
